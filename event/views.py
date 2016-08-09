e109045d181227cf1a26a0010487a7dbeb21f941
ncailleux <n.cailleux.pro@gmail.com>
2016-08-09 16:00:58 +0100

    add event per day list & ohters

--- event/views.py
+++ event/views.py
@@ -7,6 +7,9 @@ from django.shortcuts import get_object_or_404
 from django.views.generic import ListView, DetailView, TemplateView
 from .models import Event, Ticket
 from django.utils import timezone
+from django.utils.dateformat import DateFormat
+from django.utils.formats import get_format
+from datetime import date, time, datetime
 
 
 class EventListView(ListView):
@@ -23,15 +26,6 @@ def Events(request):
     return render(request, 'event_list.html', {'eventList': eventlist, })
 
 
-# def EventDetails(request, id_Event):
-#     c = {}
-#     event = get_object_or_404(Event, pk=id_Event)
-#     #ticketHere = get_object_or_404(ticket, pk = id_Event)
-#     listTik = Ticket.objects.filter(event_id=id_Event)
-#
-#     return render(request, 'event_detail.html/'.format(id_Event), {'listTik': listTik, 'event': event}, c)
-
-
 class EventDetail(DetailView):
     template_name = 'event_detail.html'
     model = Event
@@ -44,3 +38,27 @@ class EventDetail(DetailView):
 
     def get_success_url(self):
         return reverse('event-detail')
+
+# def EventDetails(request, id_Event):
+#     c = {}
+#     event = get_object_or_404(Event, pk=id_Event)
+#     #ticketHere = get_object_or_404(ticket, pk = id_Event)
+#     listTik = Ticket.objects.filter(event_id=id_Event)
+#     return render(request, 'event_detail.html/'.format(id_Event), {'listTik': listTik, 'event': event}, c)
+
+# def EventDay(request, year, month, day):
+#     y = int(year)
+#     m = int(month)
+#     d = int(day)
+#     dateDay = datetime(y, m, d)
+#     model = Event
+#     #for eve in Event:
+#       #  dateTest = eve.date_start|date:'Y/m/d'
+#        # if (dateTest == dateDay):
+#         #    listEventDay += get_object_or_404(Event, date_start = eve.date_start)
+#         #endif
+#     #endfor
+#     #eventDay = Event.objects.filter(date_start = dateDay)
+#     eventDay = Event.objects.filter(visibility=1)
+#     return render(request, 'event_day.html', {'listEventDay': listEventDay, })
+
